2e7d93f7bc2167111e035c3426e4cba5c5b8119d
Bao Toan <s4045102@rmit.edu.vn>
2025-05-22 23:19:53 +0700

    javafx ran  successfully with mvn clean javafx:run

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+public class Main {
+    private static volatile boolean ready = false;
+    public static void main( String[] args ) {
+        Thread backendThread = new Thread(() -> {
+            // final EntityManagerFactory emf = JPAUtil.getEntityManagerFactory();
+            // EntityManager em = emf.createEntityManager();
+            // if (em != null && emf != null) {
+            //     System.out.println("Successfully connected to database");
+            // }
+            System.out.println("Backend is running...");
+            ready = true;
+        });
+        backendThread.setDaemon(true);
+        backendThread.start();
+        // Launch JavaFX application on the JavaFX Application Thread
+        App.main(args); // calls Application.launch() Uncomment to run javafx
+    }
+
+    public static boolean backendReady() {
+        return ready;
+    }
+}
